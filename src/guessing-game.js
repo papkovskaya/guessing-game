041f896f5b4273537f0c3363723d3dfe9f3fbff5
Olga Papkovskaya <o.papkovskaya@gmail.com>
2017-01-29 21:37:27 +0300

    check

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = this.right = this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.left + this.right) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.right = this.middle;
     }
 
     greater() {
-
+        this.left = this.middle;
     }
 }
 
